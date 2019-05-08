0687bf9205bb435b41790c5f5f3a939da686bda9
toalba <tb@kleinundpartner.at>
2019-05-08 11:36:46 +0200

    Slave&Master

--- Hintereinanderschaltung_parrallel_/Hintereinanderschaltung_parrallel_.ino
+++ Hintereinanderschaltung_parrallel_/Hintereinanderschaltung_parrallel_.ino
@@ -1,4 +1,4 @@
-
+#include <Arduino.h>
 const int led1=7;
 const int led2=6;
 const int led3=5;
---
+++ MasterandSlave/Slave.ino
@@ -0,0 +1,32 @@
+// Wire Slave Receiver
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+}
+
+void loop() {
+  delay(x)
+  
+  
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
